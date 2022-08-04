512646b063093ad1922439fbba82d5e88bc09b61
unknown <wealthymanyasa@gmail.com>
2022-08-04 15:19:44 +0200

    added the resource route

--- routes/api.php
+++ routes/api.php
@@ -19,14 +19,17 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-//get all the posts
-Route::get('/posts', [PostController::class,'index']);
-//create post
-Route::post('/post', [PostController::class,'store']);
-//get single post
-Route::get('/posts/{id}', [PostController::class,'show']);
-//update ppost
-Route::put('/posts/{id}', [PostController::class,'update']);
-//delete post
-Route::delete('/posts/{id}', [PostController::class,'destroy']);
+// //get all the posts
+// Route::get('/posts', [PostController::class,'index']);
+// //create post
+// Route::post('/post', [PostController::class,'store']);
+// //get single post
+// Route::get('/posts/{id}', [PostController::class,'show']);
+// //update ppost
+// Route::put('/posts/{id}', [PostController::class,'update']);
+// //delete post
+// Route::delete('/posts/{id}', [PostController::class,'destroy']);
+
+//simply use this for convenience
+ Route::resource('posts', PostController::class);
 
